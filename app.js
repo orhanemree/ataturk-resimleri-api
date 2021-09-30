576ca290b7246c29ff56a42e15d90026a77f0c5d
Orhan Emre <orhanemre.dev@gmail.com>
2021-09-30 20:34:49 +0300

    add 40 images

---
+++ app.js
@@ -0,0 +1,31 @@
+const Express = require("express");
+const app = Express();
+const images = require("./images.json");
+const cors = require("cors");
+
+app.use(cors());
+app.use(Express.static(__dirname+'/public'));
+
+app.get("/", (req, res) => {
+    res.status(200).json(images[Math.floor(Math.random() * images.length)]);
+});
+
+app.get("/resim", (req, res) => {
+    res.status(200).send(`<img style="width: 50%;" src="/images/${images[Math.floor(Math.random() * images.length)].id}.png" alt="ataturk"></img><style>body{background-color: red; margin: 0px; background: #0e0e0e; display: grid; place-items: center;</style>`);
+});
+
+app.get("/:id", (req, res) => {
+    if (images[(req.params.id)-1] === undefined){
+        res.status(404).json({"404-not-found": `Bu id'ye sahip bir resim yok yanlızca 1-${images.length} arasında bir sayı girilebilir.`})
+    }
+    res.status(200).json(images[(req.params.id)-1]);
+});
+
+app.get("/:id/resim", (req, res) => {
+    if (images[(req.params.id)-1] === undefined){
+        res.status(404).json({"404-not-found": `Bu id'ye sahip bir resim yok yanlızca 1-${images.length} arasında bir sayı girilebilir.`})
+    }
+    res.status(200).send(`<img style="width: 50%;" src="/images/${req.params.id}.png" alt="ataturk"></img><style>body{background-color: red; margin: 0px; background: #0e0e0e; display: grid; place-items: center;</style>`);
+});
+
+app.listen(8080);
