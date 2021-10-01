9f7a650ff2cab5a95c493b3ba9a0bf2d8e7dc3dc
Orhan Emre <orhanemre.dev@gmail.com>
2021-10-01 19:26:15 +0300

    heroku deploy

--- app.js
+++ app.js
@@ -28,4 +28,7 @@ app.get("/:id/resim", (req, res) => {
     res.status(200).send(`<img style="width: 50%;" src="/images/${req.params.id}.png" alt="ataturk"></img><style>body{background-color: red; margin: 0px; background: #0e0e0e; display: grid; place-items: center;</style>`);
 });
 
-app.listen(8080);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.warn(`App listening on http://localhost:${PORT}`);
+});
